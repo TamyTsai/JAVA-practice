4036fb28f4adae71be52af3927b946dbb7541408
Tamy <tamy8677@gmail.com>
2024-11-19 22:31:54 +0800

    輸出

---
+++ basic_note/output.java
@@ -0,0 +1,13 @@
+// package basic_note;
+
+// 簡單輸出
+
+void main() {
+	System.out.print("小白");
+	System.out.print("小藍");
+	System.out.print("123");
+}
+
+// 沒有嚴格縮排 而且有分號 所以全部程式碼都寫在同一行沒空格也可以
+// print預設最後非換行符號
+// 分號表示敘述結束
