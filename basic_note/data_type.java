04e1ca6b83b429b2be4d5e23d970e47da8906b8b
Tamy <tamy8677@gmail.com>
2024-11-28 18:47:19 +0800

    資料型態

---
+++ basic_note/data_type.java
@@ -0,0 +1,24 @@
+// package basic_note;
+
+// public class data_type {
+
+// }
+
+// 資料型態
+void main() {
+    // 字串: "小綠" (一段純文字)
+    System.out.println("小綠");
+    System.out.println("小綠"); // println最後會換行
+    // 字元: '王' (一個純文字)(單引號)(不能寫超過一個的文字，超過一個會報錯)
+    System.out.println('王');
+    // 整數: 18
+    System.out.println(18);
+    System.out.println(18 + 16); // 整數可以做運算，字串不能
+    System.out.println("18" + "16");
+    System.out.println(18 * 16);
+    // 浮點數: 160.5
+    System.out.println(160.5);
+    // 布林值: true false (是、否)(真、假)
+    System.out.println(true);
+    System.out.println(false);
+}
