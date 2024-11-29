d3c38f20c55e61530bb20f051e24a27c7e3ee752
Tamy <tamy8677@gmail.com>
2024-11-29 21:53:29 +0800

    變數

---
+++ basic_note/variable.java
@@ -0,0 +1,43 @@
+// package basic_note;
+// public class variable {
+    
+// }
+
+// 變數
+
+void main() {
+	// 靜態型別 程式撰寫時需明確的型別宣告
+	// 字串: "小綠"
+	String name = "小綠"; // 要先定義 後面的程式碼才能使用變數
+	System.out.println(name);
+	name = "小黃"; // 變數內的值改變
+	System.out.println(name);
+	// 字元: '王'
+	char lastName = '王';
+	System.out.println(lastName);
+	lastName = '黃';
+	System.out.println(lastName);
+	// 整數: 18
+	int age = 18;
+	System.out.println(age);
+	age = 25;
+	System.out.println(age);
+	// 浮點數: 160.5
+	double height = 160.5;
+	System.out.println(height);
+	// 布林值: true false 
+	boolean isMale = true;
+	System.out.println(isMale);
+	isMale = false;
+	System.out.println(isMale);
+}
+
+// 變數命名規則
+
+// 1. 只能是字母、數字、_、$的組合
+// 2. 開頭不能是數字
+// 3. 不能是保留字
+// 4. 命名習慣 myName、myFavoritFood(駝峰式命名法)
+// 5. 取有意義的名字
+
+
